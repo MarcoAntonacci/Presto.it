137ba19a7ea01ec92c79d1d77c4703c684a23739
Davide Cariola <davidecariola90@gmail.com>
2021-04-27 14:53:20 +0200

    Navbar completa

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,44 +1,49 @@
-<nav class="navbar navbar-expand-lg bg-nav navbar-light shadow fixed-top">
-    <div class="container-fluid ">
-      {{-- <img src="/img/presto-logo.png" class="mini-logo" alt="Presto.it"> --}}
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mb-2 mb-lg-0 mx-auto">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="{{route('homepage')}}">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{route('ad.index')}}">I nostri annunci!</a>
-          </li>
-          @auth
-          <li class="nav-item">
-            <a class="nav-link" href="{{route('ad.create')}}">Inserisci il tuo annuncio!</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Benvenuto, {{Auth::user()->name}}!
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li><a class="dropdown-item" href="#">Profilo</a></li>
-              <li><a class="dropdown-item" href="{{route('logout')}}" onclick="event.preventDefault(); document.getElementById('form-logout').submit();">Esci</a></li>
-              <form method="POST" action="{{route('logout')}}" style="display: none" id="form-logout">
-                @csrf
-              </form>
-            </ul>
-            @else
+<header>
+    <div class="bg-nav">
+      <img src="/img/presto-logo.png" class="mini-logo mx-auto" alt="Presto.it">
+    </div>
+  <nav class="navbar navbar-expand-lg bg-nav navbar-light shadow-nav">
+      <div class="container-fluid ">
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mb-2 mb-lg-0 mx-auto">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="{{route('homepage')}}">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{route('ad.index')}}">I nostri annunci!</a>
+            </li>
+            @auth
+            <li class="nav-item">
+              <a class="nav-link" href="{{route('ad.create')}}">Inserisci il tuo annuncio!</a>
+            </li>
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Benvenuto, Ospite!
+                Benvenuto, {{Auth::user()->name}}!
               </a>
               <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="{{route('register')}}">Registrati</a></li>
-                <li><a class="dropdown-item" href="{{route('login')}}">Accedi</a></li>
+                <li><a class="dropdown-item" href="#">Profilo</a></li>
+                <li><a class="dropdown-item" href="{{route('logout')}}" onclick="event.preventDefault(); document.getElementById('form-logout').submit();">Esci</a></li>
+                <form method="POST" action="{{route('logout')}}" style="display: none" id="form-logout">
+                  @csrf
+                </form>
               </ul>
-              @endauth
-          </li>
-        </ul>
+              @else
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Benvenuto, Ospite!
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="{{route('register')}}">Registrati</a></li>
+                  <li><a class="dropdown-item" href="{{route('login')}}">Accedi</a></li>
+                </ul>
+                @endauth
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
+  </header>
