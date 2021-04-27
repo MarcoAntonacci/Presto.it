83b453524d89945208c18f7bc8079124fb45b86c
Giovanni Omettere <omettere@gmail.com>
2021-04-27 11:11:05 +0200

    modifiche allo stile

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,10 @@
 <x-layout>
-    <h1 class="tc-accent">Presto.it</h1>
-    <div class="container">
-        <div class="row">
+    <div class="container mt-5">
+        <div class="row row row-cols-2 row-cols-lg-5 g-2 g-lg-3">
             @foreach ($categories as $category)
-            <div class="col-12 col-md-4">
+            <div class="col-12 col-md-2">
                 <div class="card">
-                    <div class="card-body">
+                    <div class="card-body card-style">
                       <a href="{{route('category', ['cat'=>$category->id])}}"><h5 class="card-title">{{$category->name}}</h5></a>
                     </div>
                 </div>
@@ -29,8 +28,8 @@
                           <a href="{{route('ad.show', compact('ad'))}}" class="btn btn-primary">Dettaglio dell'annuncio</a>
                         </div>
                       </div>
+                    </div>
                 </div>
-            </div>
             </div>
         @endforeach
     </div>
