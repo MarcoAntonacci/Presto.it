64cadfa427f562aea9a003fe0c667cce4d8d5450
Marco Antonacci <marco.antonacci96@libero.it>
2021-05-03 18:25:02 +0200

    mobile pagina revisore

--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -8,16 +8,10 @@
             <div class="mb-3">
                 <div class="row align-items-center justify-content-center">
 
-                  {{--Tasto Reject  --}}
-                  <div class="col-1">
-                    <form method="POST" action="{{route('revisor.reject', $ad->id)}}">
-                      @csrf
-                          <button type="submit" class="btn btn-danger">Rifiuta</button>
-                    </form>
-                  </div>
-                  
+
+
                     {{-- Carousel --}}
-                    <div class="col-md-5">
+                    <div class="col-12 col-md-5">
                       <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
                           <div class="carousel-indicators">
                             <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -45,8 +39,8 @@
                     </div>
 
                     {{-- Dati --}}
-                    <div class="col-md-3">
-                      <div class="card-body ms-5">
+                    <div class="col-12 col-md-3">
+                      <div class="card-body">
                         <h5 class="card-title fs-2">{{$ad->title}}</h5>
                         <p class="card-text tc-accent fs-3">€ {{$ad->price}}</p>
                         <a href="{{route('category', ['cat'=>$ad->category->id])}}"><p class="card-text tc-black">{{$ad->category->name}}</p></a>
@@ -54,26 +48,36 @@
                       </div>
                     </div>
 
-                  {{-- Tasto Accept --}}
-                  <div class="col-md-1">
-                    <form method="POST" action="{{route('revisor.accept', $ad->id)}}">
-                      @csrf
-                          <button type="submit" class="btn btn-success">Accetta</button>
-                    </form>
-                  </div>
                 </div>
               </div>
       </div>
+      {{-- Descrizione --}}
       <div class="row justify-content-center">
-        <div class="col-6 card">
+        <div class="col-11 col-md-6 card">
           <p class="card-text text-justify">{{$ad->description}}</p>
         </div>
-        <div class="row justify-content-center mt-4">
-          <div class="text-center">
-            <a href="{{route('revisor.trash')}}" class="btn btn-primary">Vai al cestino</a>
-          </div>
-        </div>
       </div>
+
+      {{-- Tasti --}}
+        <div class="row justify-content-center mt-4">
+          <div class="col-12 col-md-2">
+            <a href="{{route('revisor.trash')}}" class="btn btn-primary mb-3">Vai al cestino</a>
+          </div>
+          <div class="col-12 col-md-2">
+            <form method="POST" action="{{route('revisor.reject', $ad->id)}}">
+                @csrf
+                    <button type="submit" class="btn btn-danger mb-3">Rifiuta</button>
+              </form>
+          </div>
+          <div class="col-12 col-md-2">
+            <form method="POST" action="{{route('revisor.accept', $ad->id)}}">
+                @csrf
+                    <button type="submit" class="btn btn-success mb-3">Accetta</button>
+              </form>
+          </div>
+
+        </div>
+
     </div>
 
 
